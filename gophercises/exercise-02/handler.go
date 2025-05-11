6c1596b4369f43c634feaf27e46d1f3bac3c209c
Patrick O'Neil <oupeng96@pm.me>
2025-05-11 12:36:36 +0800

    Update exercise 2 with stub files from gophercise repo

--- gophercises/exercise-02/handler.go
+++ gophercises/exercise-02/handler.go
@@ -1,7 +1,40 @@
+// This is the primary file for completing gophercises exercise 2.
+// Original stub file downloaded from:
+// https://github.com/gophercises/urlshort/blob/master/handler.go
 package urlshortener
 
-import "fmt"
+import (
+	"net/http"
+)
 
-func ExportedMethod() {
-	fmt.Println("This doesn't do anything yet!")
+// MapHandler will return an http.HandlerFunc (which also
+// implements http.Handler) that will attempt to map any
+// paths (keys in the map) to their corresponding URL (values
+// that each key in the map points to, in string format).
+// If the path is not provided in the map, then the fallback
+// http.Handler will be called instead.
+func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
+	//	TODO: Implement this...
+	return nil
+}
+
+// YAMLHandler will parse the provided YAML and then return
+// an http.HandlerFunc (which also implements http.Handler)
+// that will attempt to map any paths to their corresponding
+// URL. If the path is not provided in the YAML, then the
+// fallback http.Handler will be called instead.
+//
+// YAML is expected to be in the format:
+//
+//   - path: /some-path
+//     url: https://www.some-url.com/demo
+//
+// The only errors that can be returned all related to having
+// invalid YAML data.
+//
+// See MapHandler to create a similar http.HandlerFunc via
+// a mapping of paths to urls.
+func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	// TODO: Implement this...
+	return nil, nil
 }
--- gophercises/exercise-02/main/main.go
+++ gophercises/exercise-02/main/main.go
@@ -1,11 +1,47 @@
+// This was downloaded from the gophercises github:
+// https://github.com/gophercises/urlshort/blob/master/main/main.go
+// Then edited by me to complete the exercise.
+// The bulk of the changes/implementation are the maphandler and YAMLhandler in ../handler.go.
 package main
 
 import (
 	"fmt"
+	"net/http"
 	"urlshortener"
 )
 
 func main() {
-	fmt.Println("Main package - not doing anything")
-	urlshortener.ExportedMethod()
+	mux := defaultMux()
+
+	// Build the MapHandler using the mux as the fallback
+	pathsToUrls := map[string]string{
+		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
+		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
+	}
+	mapHandler := urlshortener.MapHandler(pathsToUrls, mux)
+
+	// Build the YAMLHandler using the mapHandler as the
+	// fallback
+	yaml := `
+- path: /urlshort
+  url: https://github.com/gophercises/urlshort
+- path: /urlshort-final
+  url: https://github.com/gophercises/urlshort/tree/solution
+`
+	yamlHandler, err := urlshortener.YAMLHandler([]byte(yaml), mapHandler)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Starting the server on :8080")
+	http.ListenAndServe(":8080", yamlHandler)
+}
+
+func defaultMux() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", hello)
+	return mux
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, world!")
 }
