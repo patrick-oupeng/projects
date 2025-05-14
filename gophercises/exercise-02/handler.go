462ad7dc61c93cbd52124c177fcc678ad7ab44b1
Patrick O'Neil <oupeng96@pm.me>
2025-05-14 16:49:41 +0800

    Expand funcationality

--- gophercises/exercise-02/handler.go
+++ gophercises/exercise-02/handler.go
@@ -4,17 +4,16 @@
 package urlshortener
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
 
 	"github.com/go-yaml/yaml"
 )
 
-// MapHandler will return an http.HandlerFunc (which also
-// implements http.Handler) that will attempt to map any
-// paths (keys in the map) to their corresponding URL (values
-// that each key in the map points to, in string format).
-// If the path is not provided in the map, then the fallback
-// http.Handler will be called instead.
+// MapHandler returns an http.HandlerFunc (which is polymophism of http.Handler)
+// which maps paths to their corresponding URL.
+// If a path is not provided, the fallback handler is called.
 func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
 	return func(respwriter http.ResponseWriter, req *http.Request) {
 		if realpath, ok := pathsToUrls[req.URL.Path]; ok {
@@ -25,44 +24,47 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 	}
 }
 
-// YAMLHandler will parse the provided YAML and then return
-// an http.HandlerFunc (which also implements http.Handler)
-// that will attempt to map any paths to their corresponding
-// URL. If the path is not provided in the YAML, then the
-// fallback http.Handler will be called instead.
-//
-// YAML is expected to be in the format:
-//
-//   - path: /some-path
-//     url: https://www.some-url.com/demo
-//
-// The only errors that can be returned all related to having
-// invalid YAML data.
-//
-// See MapHandler to create a similar http.HandlerFunc via
-// a mapping of paths to urls.
-type myYaml struct {
+// A struct as a go-between for Unmarshal and maps.
+type myStruct struct {
 	Path string `yaml:"path"`
 	URL  string `yaml:"url"`
 }
 
+// Wrapper (terminology?) for yaml.Unmarshal and json.Unmarshal
+type unmarshalfunc func(in []byte, out interface{}) (err error)
+
 func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
-	yamlList := []myYaml{}
-	err := yaml.Unmarshal(yml, &yamlList)
+	log.Println("Parsing YAML...")
+	return abstractHandler(yml, fallback, yaml.Unmarshal)
+}
+
+func JSONHandler(jsn []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	log.Println("Parsing JSON...")
+	return abstractHandler(jsn, fallback, json.Unmarshal)
+}
+
+// Unmarshals the given data using the given unmarshal function
+// then converts into a map and uses the maphandler.
+func abstractHandler(hierarchy []byte, fallback http.Handler, unmarshal unmarshalfunc) (http.HandlerFunc, error) {
+	log.Println("Unmarshalling the data")
+	unmarshalList := []myStruct{}
+	err := unmarshal(hierarchy, &unmarshalList)
 	if err != nil {
 		return nil, err
 	}
 
-	mymap, err := yamltomap(yamlList)
+	log.Println("Converting from a list of structs into a map")
+	mymap, err := structtomap(unmarshalList)
 	if err != nil {
 		return nil, err
 	}
 	return MapHandler(mymap, fallback), nil
 }
 
-func yamltomap(mylist []myYaml) (map[string]string, error) {
+func structtomap(mylist []myStruct) (map[string]string, error) {
 	retmap := make(map[string]string)
 	for _, val := range mylist {
+		log.Printf("Mapping %s to %s\n", val.Path, val.URL)
 		retmap[val.Path] = val.URL
 	}
 	return retmap, nil
--- gophercises/exercise-02/main/main.go
+++ gophercises/exercise-02/main/main.go
@@ -5,38 +5,87 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 	"urlshortener"
 )
 
-func main() {
-	mux := defaultMux()
+var yamlfile *string
+var jsonfile *string
 
-	// Build the MapHandler using the mux as the fallback
-	pathsToUrls := map[string]string{
-		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
-		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
-	}
-	mapHandler := urlshortener.MapHandler(pathsToUrls, mux)
+var map_sample = map[string]string{
+	"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
+	"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
+}
 
-	// Build the YAMLHandler using the mapHandler as the
-	// fallback
-	yaml := `
+var yaml_sample string = `
 - path: /urlshort
   url: https://github.com/gophercises/urlshort
 - path: /urlshort-final
   url: https://github.com/gophercises/urlshort/tree/solution
 `
-	yamlHandler, err := urlshortener.YAMLHandler([]byte(yaml), mapHandler)
+var json_sample string = `
+[
+    {
+        "path": "/mytest",
+        "url": "https://abair.ie/"
+    },
+    {
+        "path": "/mytest-2",
+        "url": "https://itaigi.tw/"
+    }
+]
+`
+
+func init() {
+	// If a json or yaml file are passed in, reads from them, otherwise uses the default samples above.
+	jsonfile = flag.String("json", "", "a .json file to load from")
+	yamlfile = flag.String("yaml", "", "a .yaml file to load from")
+	flag.Parse()
+	if *jsonfile != "" {
+		file_to_string(*jsonfile, &json_sample)
+		log.Println("Using json file", *jsonfile)
+	}
+	if *yamlfile != "" {
+		file_to_string(*yamlfile, &yaml_sample)
+		log.Println("Using yaml file", *yamlfile)
+	}
+}
+
+func file_to_string(filename string, read_into *string) {
+	file, err := os.ReadFile(filename)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("Starting the server on :8080")
-	http.ListenAndServe(":8080", yamlHandler)
-	// http.ListenAndServe(":8080", mapHandler)
+	*read_into = string(file)
+	// defer file.Close() // not needed for ReadFile
 }
 
+func main() {
+	mux := defaultMux()
+
+	// Build the MapHandler using the mux as the fallback
+	mapHandler := urlshortener.MapHandler(map_sample, mux)
+
+	// Build the YAMLHandler using the mapHandler as the
+	// fallback
+	yamlHandler, err := urlshortener.YAMLHandler([]byte(yaml_sample), mapHandler)
+	if err != nil {
+		log.Panic(err)
+	}
+	// Same with json
+	jsonHandler, err := urlshortener.JSONHandler([]byte(json_sample), yamlHandler)
+	if err != nil {
+		log.Panic(err)
+	}
+	log.Println("Starting the server on :8080")
+	http.ListenAndServe(":8080", jsonHandler)
+}
+
+// Default functions provided in the example code.
 func defaultMux() *http.ServeMux {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", hello)
